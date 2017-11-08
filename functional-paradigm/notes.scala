0a9cb268f74d0b5a0c53adef071ed87c73e13538
Lucas Santos <lucas.santos.silva2011@gmail.com>
2017-11-07 23:15:16 -0200

    notes: added some new notes just to keep them in mind

---
+++ functional-paradigm/notes.scala
@@ -0,0 +1,33 @@
+/*
+  This file contains some notes about the Scala syntax
+*/
+
+// "for-loop"
+def printName(name: String, times: Int) {
+  def loop(n: Int): Unit = {
+    if(n > times) return
+    else {
+      println(name)
+      loop(n + 1)
+    }
+  }
+
+  loop(1)
+}
+
+printName("Hello world", 10) // prints 'hello world' 10 times
+
+/*
+ sum function
+ This function takes a integer as argument and returns another
+ function that takes an integer as parameter 
+ and returns another integer, the sum of two numbers.
+*/
+def sum(n: Int): Int => Int = {
+  def n2(x: Int): Int = {
+    n + x
+  }
+  n2
+}
+
+sum(5) (3) // sums 5 and 3, returning 8
